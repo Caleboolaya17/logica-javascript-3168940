c962c29cf08a48e275b4eb3392f7b4e28bace142
Caleboolaya17 <fernandoolaya086@gmail.com>
2025-07-31 12:26:00 -0500

    repositorio js versión 1

---
+++ 2_operadores_comparacion_logico/app.js
@@ -0,0 +1,26 @@
+//Boolean(...)
+
+console.log(' "5" == "5" ', "5" == "5") //true
+console.log(' "5" === "5" ', "5" === "5") //true
+console.log(' 5 === "5" ', 5 === "5") //false
+console.log(' 5 == "5" ', 5 == "5") //true
+console.log(' 5 === (3+2) ', "5" === (3+2)) //false
+console.log(' 5 === 5 ', 5 === 5) //true
+
+//Concatenar ( + )
+
+console.log( 5 + "3")
+console.log( 5 + "3")
+console.log( 5 + "3")
+
+//pida dos numeros al usuaio y sumelos
+let num1 = prompt( 'dame num 1' )
+let num2 = prompt( 'dame num 2' )
+let resultado = num1 + num2
+alert( " El resultado de tu suma es " + resultado)
+
+
+let num4 = prompt( 'dame num 1' )
+let num3 = prompt( 'dame num 2' )
+let result = Number(num1) + Number(num2)
+alert( '"{num1}" + "{num2}" == "resultado"' )
