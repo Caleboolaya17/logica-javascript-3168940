eae6b0d8ef332ee3b8782a3c123ce0026de8582f
Caleboolaya17 <fernandoolaya086@gmail.com>
2025-09-16 13:11:42 -0500

    Add files via upload

---
+++ galeria_1/app.js
@@ -0,0 +1,37 @@
+const titulo = document.querySelector(".titulo")
+const contenedor = document.querySelector("#contenedor")
+const btnPrev = document.querySelector(".btn-prev")
+const btnNext = document.querySelector(".btn-next")
+const titulos = [
+  "Primera imagen",
+  "Segunda imagen",
+  "Tercera imagen",
+  "Cuarta imagen",
+  "Quinta imagen",
+]
+let i = 1
+
+function prev() {
+  console.log("Anterior....")
+  if (i === 0) {
+    i = 5
+  }
+  i-- //i=4
+  titulo.textContent = titulos[i]
+  contenedor.style.marginLeft = `-${400 * i}px`
+  contenedor.style.transitionDuration = ".5s"
+}
+
+function next() {
+  console.log(i)
+  if (i === 5) {
+    i = 0
+  }
+  titulo.textContent = titulos[i]
+  contenedor.style.marginLeft = `-${400 * i}px`
+  contenedor.style.transitionDuration = ".5s"
+  i++ //i=1
+}
+
+btnPrev.addEventListener("click", prev)
+btnNext.addEventListener("click", next)
